cd1e3223ccc79d960c12bf4438849a8d76998cc3
JOELTORCATO <a12022@ae4outubro.pt>
2024-02-23 11:27:24 +0000

    fourth commit.

--- 19 FEB/PERIMETER AND AREA OF A CIRCLE/perimeter and area of a circle.c
+++ 19 FEB/PERIMETER AND AREA OF A CIRCLE/perimeter and area of a circle.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float raio, perimetro, area;
-    printf("what is the radius: "); scanf("%f", &raio);
+    float raio, perimetro;
+    double area;
+    printf("what is the radius: ");
+    scanf("%f", &raio);
     perimetro = 2 * 3.14159 * raio;
     area = 3.14159 * raio * raio;
     printf("the perimeter is %f and the area is %f\n", perimetro, area);
