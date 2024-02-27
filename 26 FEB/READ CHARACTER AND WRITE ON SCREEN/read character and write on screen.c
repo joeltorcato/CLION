8885f23f9c8fe93033f61d1e478f1e25ff896a7c
dev joel <154090684+joeltorcato@users.noreply.github.com>
2024-02-27 09:10:30 +0000

    new commit

---
+++ 26 FEB/READ CHARACTER AND WRITE ON SCREEN/read character and write on screen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("type a character: ");
+    scanf("%c", &ch);
+    printf("the character you typed is: %c\n", ch);
+}
---
+++ 26 FEB/REQUEST AN INTEGER (BETWEEN 0 AND 255)/request an integer (between 0 and 255).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+        printf("writes an ASCII number between 0 and 255: ");
+        scanf("%d", &num);
+        printf("the symbol is = %c",
+               num,(char)num);
+    printf("the charcter is = %c" tem o ASCII number %d\n",
+            (char) (num+1) , num+1);
+
+}
