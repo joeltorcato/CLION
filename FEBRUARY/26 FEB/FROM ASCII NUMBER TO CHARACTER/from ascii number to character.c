15a1216e22a8ed09c7549e4574ec42b2d39492fe
JOELTORCATO <a12022@ae4outubro.pt>
2024-03-09 13:27:35 +0000

    new commit 09/03/2024

--- FEBRUARY/26 FEB/FROM ASCII NUMBER TO CHARACTER/from ascii number to character.c
+++ FEBRUARY/26 FEB/FROM ASCII NUMBER TO CHARACTER/from ascii number to character.c
@@ -2,9 +2,8 @@
 
 int main() {
     int num;
+
         printf("writes an ASCII number between 0 and 255: ");
         scanf("%d", &num);
-        printf("the symbol is = %c",
-               num,(char)num);
-    printf("the charcter is = %c tem o ASCII number %d\n");
+        printf("the number %d in ASCII is the character %c\n", num, num);
 }
