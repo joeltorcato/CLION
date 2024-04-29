45d36b33f4847a035e8d242f9abacb8f6f891afb
dev joel <154090684+joeltorcato@users.noreply.github.com>
2024-04-29 17:55:33 +0100

    new commit. 29/04/2024.

--- APRIL/29 APL/WHAT IS THE OUTPUT OF THE FOLLOWING PROGRAM 2/what is the output of the following program 2.c
+++ APRIL/29 APL/WHAT IS THE OUTPUT OF THE FOLLOWING PROGRAM 2/what is the output of the following program 2.c
@@ -3,10 +3,13 @@
 int main () {
     int i;
     for (i = 1 ; i <= 100 ; i = i + 1) {
-        if (i == 30)
+        if (i == 60)
             break;
         else
-            printf("%d\n", 2 * i);
+            if (i % 2 == 1)
+                continue;
+            else
+            printf("%2d\n", 2 * i);
         printf("END OF THE LOOP\n");
     }
 }
